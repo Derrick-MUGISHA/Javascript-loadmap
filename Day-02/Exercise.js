a97becb6b5577e13b86df24f5c59a16bc3c7214b
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2026-05-19 19:55:51 +0200

    we studed in day 2 the scope datatype and declaration in javascript

---
+++ Day-02/Exercise.js
@@ -0,0 +1,40 @@
+
+
+// Question 1 creating Variables
+let name = "Derrick"; 
+let age = 30;
+let country = "Rwanda";
+let likeJavaScript = true;
+
+console.log("My name is " + name + ", I am " + age + " years old, I live in " + country + " and I like JavaScript: " + likeJavaScript);
+
+// Question 2
+
+let score = 85;
+score = 90;
+score = 55;
+
+// Question 3 
+
+const PI = 3.14159;
+// PI = 3.14;
+// console.log(PI);  Assigned to a constant Variable 
+
+
+// Question 4
+
+let firstName = "Derrick";  // these is what we call camelCase naming convention
+let user_age = 30;
+let $price = 100;
+
+//invalid
+// let FirstName= Derrick;
+// let user_age= 30;
+
+// Question 5
+
+let x = 10;
+
+console.log(typeof x);
+
+
